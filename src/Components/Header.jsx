1ba8dd94ffb827c7b98a062271270ac92c67d790
Aastha kalra <aasthakalra2004@gmail.com>
2024-02-12 12:50:09 +0530

    Header file

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,139 +1,155 @@
-import React, { useState, useEffect } from "react";
-import { Link } from 'react-scroll';
+import React, { useState } from "react";
+import { Link } from "react-scroll";
 const Header = () => {
-  const [showNavbar, setShowNavbar] = useState(false);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const handleScroll = () => {
-    if (window.scrollY > 100) {
-      setShowNavbar(true);
-    } else {
-      setShowNavbar(false);
-    }
-  };
-
   const [open, setOpen] = useState(false);
   return (
     <>
       <div
-        className={`bg-[#1F252A]  fixed top-0 xl:flex
-        
+        className={`bg-[#1F252A] fixed top-0 xl:flex  
         justify-around
-        xl:justify-cente items-center h-16 w-full  duration-300 ease-in-out text-white z-50 order-1`}
+        xl:justify-center items-center h-16 w-full  duration-300 ease-in-out text-white z-50 order-1`}
       >
-        <div className="flex justify-between items-center  w-full px-4 py-3 ">
-         <img src="Images/logo.png" alt="" />
-          <div className="text-3xl pt-3 xl:hidden" onClick={() => setOpen(!open)}>
+        <div className="flex justify-between items-center px-4 py-3 ">
+          <img src="Images/logo.png" alt="" />
+          <div
+            className="text-3xl pt-3 xl:hidden"
+            onClick={() => setOpen(!open)}
+          >
             <ion-icon name={open ? "close" : "menu"}></ion-icon>
           </div>
         </div>
 
-        <div>
+        <div className="flex w-full">
           <ul
-            className={`xl:gap-5 items-center xl:justify-center flex  bg-[#22395d] h-fit flex-col xl:flex-row w-full mt-[4px] gap-16 xl:py-10 py-12  absolute
+            className={`xl:gap-5 items-center xl:justify-center flex  bg-[#22395d] h-fit flex-col xl:flex-row w-full mt-[4px] gap-16 xl:py-10 py-12  absolute xl:overflow-hidden overflow-y-scroll
           xl:static
           z-[-1] xl:z-10 xl:text-xl text-2xl  transition-all duration-500 ease-in xl:bg-inherit ${
-            open ? "top-14" : "top-[-790px]"
+            open ? "top-14" : "top-[-1190px]"
           }`}
+            style={{ maxHeight: open ? "calc(100vh - 60px)" : "0" }}
           >
             <Link
-              to="home"
+              to="propertyShow"
               activeClass="active"
               spy={true}
               smooth={true}
               offset={-70}
               duration={500}
-              className="cursor-pointer hover:text-sky-600"
             >
-              HOME
+              Property Show
             </Link>
             <Link
-              to="about"
+              to="buy"
               activeClass="active"
               spy={true}
               smooth={true}
               offset={-70}
               duration={500}
-              className="cursor-pointer hover:text-sky-600"
             >
-              ABOUT
+              Buy
             </Link>
             <Link
-              to="features"
+              to="rent"
               activeClass="active"
               spy={true}
               smooth={true}
               offset={-70}
               duration={500}
-              className="cursor-pointer hover:text-sky-600"
             >
-             FEATURES
+              Rent
             </Link>
             <Link
-              to="clients"
+              to="sell"
               activeClass="active"
               spy={true}
               smooth={true}
               offset={-70}
               duration={500}
-              className="cursor-pointer hover:text-sky-600"
             >
-              CLIENTS
+              Sell
             </Link>
             <Link
-              to=""
+              to="newLaunch"
               activeClass="active"
               spy={true}
               smooth={true}
               offset={-70}
               duration={500}
-              className="bg-sky-500 text-2xl font-bold px-4 py-1 cursor-pointer  hover:bg-sky-600
-              hidden xl:block
-              "
             >
-              SMART ENERGY
+              New Launch
             </Link>
             <Link
-              to="team"
+              to="businessSpace "
               activeClass="active"
               spy={true}
               smooth={true}
               offset={-70}
               duration={500}
-              className="cursor-pointer hover:text-sky-600"
             >
-              TEAM
+              Business Space
             </Link>
             <Link
-              to="blogs"
+              to="overseas"
               activeClass="active"
               spy={true}
               smooth={true}
               offset={-70}
               duration={500}
-              className="cursor-pointer hover:text-sky-600"
             >
-            BLOGS
+              Overseas
             </Link>
             <Link
-              to="contact"
+              to="consultancy"
               activeClass="active"
               spy={true}
               smooth={true}
               offset={-70}
               duration={500}
-              className="cursor-pointer hover:text-sky-600"
             >
-              CONTACT US
+              Consultancy
+            </Link>
+            <Link
+              to="watch+"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Watch+
+            </Link>
+            <Link
+              to="News"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              News
+            </Link>
+            <Link
+              to="login"
+              activeClass="active"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Login
             </Link>
           </ul>
         </div>
+      </div>
+
+      <div className="bg-[#E6E9EC] hidden  h-16 text-[#303841] mt-14 lg:flex justify-center items-center ">
+        <span className="text-[#113DFA] font-medium"> Click here</span> &nbsp;
+        to purchase and own the limited-edition Gold Champion CEO Figurine!
+        Avaiable from December 2021.
+      </div>
+      <div className="bg-[#E6E9EC] lg:hidden h-16 text-[#303841] mt-14 flex justify-center items-center ">
+        <span className="text-[#113DFA] font-medium"> Click here</span> &nbsp;
+        to purchase and own
       </div>
     </>
   );
